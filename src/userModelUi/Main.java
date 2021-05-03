4b68fc6cdd71dbe67cde786553eb3a437d2d7619
fighteros <am5419@fayoum.edu.eg>
2021-05-03 23:15:34 +0200

    added icon

--- src/userModelUi/Main.java
+++ src/userModelUi/Main.java
@@ -3,6 +3,7 @@ package userModelUi;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,6 +16,7 @@ public class Main extends Application {
         Scene signInScene = new Scene(fxmlLoader.load(), 800, 500);
         primaryStage.setScene(signInScene);
         primaryStage.setTitle("Sign In");
+        primaryStage.getIcons().add(new Image("resources/log-out-.png"));
         primaryStage.show();
     }
 
