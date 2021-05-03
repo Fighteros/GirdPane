4d77f10a31d560cd8c657c0872367149bb689a79
fighteros <am5419@fayoum.edu.eg>
2021-05-03 23:43:02 +0200

    finished signUp form

--- src/userModelUi/Main.java
+++ src/userModelUi/Main.java
@@ -1,22 +1,37 @@
 package userModelUi;
 
+import com.sun.istack.internal.NotNull;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main extends Application {
+    private Image ico;
+
     @Override
-    public void start(Stage primaryStage) throws IOException {
+    public void start(Stage primaryStage) throws FileNotFoundException, IOException {
         FXMLLoader fxmlLoader = new FXMLLoader();
+        FXMLLoader regsLoader = new FXMLLoader();
+        try {
+            ico = new Image("resources/log-out-.png");
+        }catch (Exception e){
+//            System.out.println("File Not Found");
+            throw new FileNotFoundException();
+        }
+
         fxmlLoader.setLocation(getClass().getResource("signIn/signIn.fxml"));
+        regsLoader.setLocation(getClass().getResource("signUp/signUp.fxml"));
         Scene signInScene = new Scene(fxmlLoader.load(), 800, 500);
+        Scene regScene = new Scene(regsLoader.load(), 800, 500);
         primaryStage.setScene(signInScene);
         primaryStage.setTitle("Sign In");
-        primaryStage.getIcons().add(new Image("resources/log-out-.png"));
+        primaryStage.getIcons().add(ico);
         primaryStage.show();
     }
 
